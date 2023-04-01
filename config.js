6889fed25e0dfa45d78c7bbfb464f51c90cebc5a
Aditya Dey <82680373+adityadey3749@users.noreply.github.com>
2023-04-01 10:47:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDH_q2J3w2C5Rz-19op-oUPXuqDDwYRedo",
+  authDomain: "banking-d3fbb.firebaseapp.com",
+  databaseURL: "https://banking-d3fbb-default-rtdb.firebaseio.com",
+  projectId: "banking-d3fbb",
+  storageBucket: "banking-d3fbb.appspot.com",
+  messagingSenderId: "1068095984162",
+  appId: "1:1068095984162:web:98ec88b52cab671e71307b"
+};
